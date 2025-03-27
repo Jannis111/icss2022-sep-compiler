33b3b6d7cabb08de71c8db8ead0f1175c23905b6
Jannis Geerts <117652582+Jannis111@users.noreply.github.com>
2025-03-27 11:23:13 +0100

    Made a start with the Evaluator.

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -9,6 +9,7 @@ import nl.han.ica.icss.ast.operations.AddOperation;
 import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.operations.SubtractOperation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -23,8 +24,52 @@ public class Evaluator implements Transform {
     @Override
     public void apply(AST ast) {
         //variableValues = new HANLinkedList<>();
-
+        applyStylesheet(ast.root);
     }
 
-    
+    private void applyStylesheet(Stylesheet root)
+    {
+        for (ASTNode child : root.getChildren())
+        {
+            if(child instanceof Stylerule)
+            {
+                applyStylerule((Stylerule) child);
+            }
+        }
+    }
+
+    private void applyStylerule(Stylerule node)
+    {
+        for (ASTNode child : node.getChildren())
+        {
+            if(child instanceof Declaration)
+            {
+                applyDeclartion((Declaration) child);
+            }
+        }
+    }
+
+    private void applyDeclartion(Declaration node)
+    {
+        node.expression = evalExpression(node.expression);
+    }
+
+    private Expression evalExpression(Expression expression)
+    {
+        if(expression instanceof Literal)
+        {
+            return (Literal)expression;
+        } else
+        {
+            return evalAddOperation((AddOperation) expression);
+        }
+    }
+
+    private Expression evalAddOperation(AddOperation addOperation)
+    {
+        PixelLiteral left = (PixelLiteral) evalExpression(addOperation.lhs);
+        PixelLiteral right = (PixelLiteral) evalExpression(addOperation.rhs);
+
+        return new PixelLiteral(left.value + right.value);
+    }
 }
